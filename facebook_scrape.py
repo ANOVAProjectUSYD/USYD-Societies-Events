20b69ec0f73c8ceb4076c11448f2f044beca924a
Chris Hyland <christopherhyland95@gmail.com>
2017-12-29 15:53:40 +0700

    Updated code.

--- facebook_scrape.py
+++ facebook_scrape.py
@@ -7,12 +7,14 @@ import requests
 def get_connected(token):
     '''Connect to the Facebook Graph API.'''
     graph = facebook.GraphAPI(access_token=token, version=2.11)
+    # Exploring SUBS Facebook page.
     subs = graph.search(type='page',q="Sydney University Business Society - SUBS")
     print(subs['data'])
     # https://developers.facebook.com/tools/explorer?method=GET&path=SydneyUniversityBusinessSociety%2Fevents%3Flimit%3D3&version=v2.11
 
 def query_graph(society_list):
     '''Return events for each society.'''
+    # TODO: Return a csv file containing information regarding events for each society.
 
 def main():
     token = 'EAACsR5scK9sBAEYiD8VoXPbJvDAOO9VQ7eRUSGTOu4YuN7tjf8KRFr9GZAhGCrU0wbFSL44VBbENKTUpdGLQXivNETGVXsvrTIRtCCPtMZC78YjYEzLeNeHksilobxRA36acDI2PMJvaCUzJppBfUtjKhhi37oX2Iv2bWadwZDZD'
