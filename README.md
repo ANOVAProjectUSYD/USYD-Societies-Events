ce14b21161b151ed491b9d0f214a60d5bb6192ba
Chris Hyland <christopherhyland95@gmail.com>
2018-01-03 21:09:27 +0700

    Started on scraping FB data for all events.

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # USYD-Societies-Events
-This project seeks to scrape Facebook data regarding events from the top 10 biggest societies at USYD. From that, we then seek to analyse what are the factors that drive event attendance.
 
-Amazing links:
-http://minimaxir.com/2015/07/facebook-scraper/
+This project seeks to scrape Facebook data regarding events from the top 10 biggest societies at USYD. From that, we then seek to analyse what are the factors that drive event attendance.
 
 This project seeks to scrape Facebook data regarding events from the top 20 biggest societies at USYD. From that, we then seek to analyse what are the factors that drive event attendance. Here, we seek to deploy models to analyse the casual relationships between event attendance and such factors such as the location of the event, timing during the semester and day, whether catering was provided, and more.
 
--- facebook_scrape.py
+++ facebook_scrape.py
@@ -1,109 +1,61 @@
-<<<<<<< HEAD
-import urllib, facebook, requests, json, datetime ,csv, time
-=======
 import urllib, facebook, requests, json, datetime, csv, time
->>>>>>> scraping-facebook
 from urllib.request import urlopen
 
 
-def grab_facebook_data(page_id, access_token, limit_status):
-    '''Access Facebook data.'''
-    # Construct the URL string
-    base = "https://graph.facebook.com/v2.4"
-    node = "/" + page_id +"/feed"
-    parameters = "/?fields=message,link,created_time,type,name,id,likes.limit(1).summary(true),comments.limit(1).summary(true),shares&limit=%s&access_token=%s" % (limit_status, access_token) # changed
-    url = base + node + parameters
-    # Retrieve data
-    data = json.loads(request_until_succeed(url))
-    return data
+def connect(page_id, token):
+    '''Connect to FB Graph API and scrape data regarding events for each society.'''
+    base = "https://graph.facebook.com/v2.11/"
+    test_file = open('Test.csv', 'w')
+    writer = csv.writer(test_file)
 
+    #TODO: Why does session keep on expiring?
+    parameters = "/?fields=events.limit(10)%7Battending_count%2Ccategory%2Cdeclined_count%2Cdescription%2Cend_time%2Cevent_times%2Cinterested_count%2Cmaybe_count%2Cname%2Cnoreply_count%2Cplace%2Cscheduled_publish_time%2Cstart_time%2Cticket_uri%2Ctype%7D&access_token=EAACEdEose0cBAAnZB6jhSiHFzwzapuTxDdDvbX8vxMOLUh2lTWIICBBZBJw2tsGr46Ea62pBLxjPO9EJlXBet8dgrsNZCBrBrMU2m20o9N7KRACM1fGRYDrPHhMzsXWx6mplOsZCWWd22lb1qjqxwU3WQuqIVpOaBDIaJYXxBSTADBM8xid0JAqQ6ZA3rtcmINDEikymjJwZDZD"
+    url = base + page_id + parameters
 
-def request_until_succeed(url):
-    '''Helps to prevent HTTP:5000 error.'''
-    response = urlopen(url)
-    success = False
-    while success is False:
+    data = json.loads(urlopen(url).read())
+    writer.writerow(["Society", "event_name", "attendance_count", "interested_count", "no_reply_count", "declined_count", "maybe_count", "total_invited_count", "description", "start_time", "end_time", "place", "event_type" ])
+
+    # What this return is a dictionary in a list in a dictionary in a dictionary.
+    for i in range(0, len(data['events']['data'])):
         try:
-            response = urlopen(url)
-            if response.getcode() == 200:
-                success = True
+            data_obj = data['events']['data'][i]
+            name = data_obj['name']
+            attendance_count = data_obj['attending_count']
+            interested_count = data_obj['interested_count']
+            no_reply_count = data_obj['noreply_count']
+            declined_count = data_obj['declined_count']
+            maybe_count = data_obj['maybe_count']
+            total_invited_count = attendance_count + interested_count + no_reply_count + declined_count + maybe_count
+            description = data_obj['description']
+            start_time = data_obj['start_time']
+            end_time = data_obj['end_time']
+            place = data_obj['place']
+            type_of_event = data_obj['type']
+
+            writer.writerow(["180DC", name, attendance_count, interested_count, no_reply_count, declined_count, maybe_count, total_invited_count, description, start_time, end_time, place, type_of_event])
         except Exception as e:
-            print(e)
-            time.sleep(5)
-            print("Error for URL %s: %s" % (url, datetime.datetime.now()))
-    return response.read()
+            continue
 
+    test_file.close()
 
-def print_data(data):
-    '''Print out the json dump given to us from testFacebookPageData.'''
-    print(json.dumps(data, indent=4, sort_keys=True))
-
-
-def process_page_status(status):
-    '''Process each field in the Facebook data.'''
-    # The status is now a Python dictionary, so for top-level items, we can simply call the key.
-    # Additionally, some items may not always exist, so must check for existence first
-    status_id = status['id']
-    status_message = '' if 'message' not in status.keys() else status['message'].encode('utf-8')
-    link_name = '' if 'name' not in status.keys() else status['name'].encode('utf-8')
-    status_type = status['type']
-    status_link = '' if 'link' not in status.keys() else status['link']
-
-    # Time needs special care since it is in UTC and not easy to use in statistical programs.
-    status_published = datetime.datetime.strptime(status['created_time'],'%Y-%m-%dT%H:%M:%S+0000')
-    status_published = status_published + datetime.timedelta(hours=-5) # EST
-    status_published = status_published.strftime('%Y-%m-%d %H:%M:%S') # Best time format for spreadsheet.
-
-    # Nested items require chaining dictionary keys.
-    num_likes = 0 if 'likes' not in status.keys() else status['likes']['summary']['total_count']
-    num_comments = 0 if 'comments' not in status.keys() else status['comments']['summary']['total_count']
-    num_shares = 0 if 'shares' not in status.keys() else status['shares']['count']
-
-    # return a tuple of all processed data
-    return (status_id, status_message, link_name, status_type, status_link,
-           status_published, num_likes, num_comments, num_shares)
-
-def scrape_facebook_page_status(page_id, access_token):
-    '''Scraping Facebook statuses for the page and writing to csv file.'''
-    with open('%s_facebook_statuses.csv' % page_id, 'w') as file:
-        w = csv.writer(file)
-        w.writerow(["status_id", "status_message", "link_name", "status_type", "status_link", "status_published", "num_likes", "num_comments", "num_shares"])
-        has_next_page = True
-        num_processed = 0   # keep a count on how many we've processed
-        scrape_starttime = datetime.datetime.now()
-        print("Scraping %s Facebook Page: %s\n" % (page_id, scrape_starttime))
-        statuses = grab_facebook_data(page_id, access_token, 10)
-        done_scraping = False
-        while has_next_page:
-            if done_scraping == True:
-                break
-            for status in statuses['data']:
-                w.writerow(process_page_status(status))
-                # Output progress occasionally to make sure code is not stalling
-                num_processed += 1
-                if num_processed == 50:
-                    # Determines how many statuses we would like to scrape.
-                    done_scraping = True
-                    break
-                if num_processed % 10 == 0:
-                    print("%s Statuses Processed: %s" % (num_processed, datetime.datetime.now()))
-
-            # Ff there is no next page, we're done.
-            if 'paging' in statuses.keys():
-                statuses = json.loads(request_until_succeed(statuses['paging']['next']))
-            else:
-                has_next_page = False
-        print("\nDone!\n%s Statuses Processed in %s" %
-             (num_processed, datetime.datetime.now() - scrape_starttime))
+def read_data():
+    '''Open CSV file and pass next entry to FB graph API.'''
+    read_file = open('final_data.csv', 'r')
+    reader = csv.reader(read_file, delimiter=',')
+    for row in reader:
+        if row[2] == "Facebook_ID":
+            continue
+        connect(row[2])
 
 
 def main():
-    token = 'EAACsR5scK9sBAEYiD8VoXPbJvDAOO9VQ7eRUSGTOu4YuN7tjf8KRFr9GZAhGCrU0wbFSL44VBbENKTUpdGLQXivNETGVXsvrTIRtCCPtMZC78YjYEzLeNeHksilobxRA36acDI2PMJvaCUzJppBfUtjKhhi37oX2Iv2bWadwZDZD'
-    # We need to access the data key and everything is stored in the first index.
-    page_id = "SydneyUniversityBusinessSociety"
-    socities_to_scrape = ["SydneyUniversityBusinessSociety", "SydneyUniversityLawSociety", "SUITSUsyd"]
-    scrape_facebook_page_status(page_id, token)
+    token = 'EAACsR5scK9sBAPzJdyZBuiw7TBqfFMwUYw7qbZAuVI2FrRegxxdfsTqFPS51wF1NFklyV6ZA5HLHz7vAjRJwzPblbnrGiY6sfulSlqZBZASf1ZBG7qQcRjozmHGLlmajZCGNqClEYHZB7EOk6HrFRuEKZAc6MpZB6fZAgoZD'
 
-    #https://developers.facebook.com/tools/explorer/145634995501895/?method=GET&path=180DegreesConsulting%3Ffields%3Devents%7Bcategory%2Cattending_count%2Ccover%2Cinterested_count%2Cevent_times%2Cend_time%2Cdescription%2Cinterested%7Babout%2Cage_range%7D%7D&version=v2.11
+    connect("180DegreesSydneyUniversity", token)
+    #read_data()
+
+
+    #"https://graph.facebook.com/v2.11/180DegreesConsulting/?fields=events.limit(10)%7Battending_count%2Ccategory%2Cdeclined_count%2Cdescription%2Cend_time%2Cevent_times%2Cinterested_count%2Cmaybe_count%2Cname%2Cnoreply_count%2Cplace%2Cscheduled_publish_time%2Cstart_time%2Cticket_uri%2Ctype%7D&access_token=EAACEdEose0cBAAnZB6jhSiHFzwzapuTxDdDvbX8vxMOLUh2lTWIICBBZBJw2tsGr46Ea62pBLxjPO9EJlXBet8dgrsNZCBrBrMU2m20o9N7KRACM1fGRYDrPHhMzsXWx6mplOsZCWWd22lb1qjqxwU3WQuqIVpOaBDIaJYXxBSTADBM8xid0JAqQ6ZA3rtcmINDEikymjJwZDZD"
+
 if __name__ == "__main__":
     main()
