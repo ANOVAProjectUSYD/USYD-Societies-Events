4ec98dc735342ec8ea5d2b8f8569b590d4d22a82
Chris Hyland <christopherhyland95@gmail.com>
2018-01-31 08:01:08 +0700

    Wrote README for Tom

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 This project seeks to scrape Facebook data regarding events from the top 20 biggest societies at USYD. From that, we then seek to analyse what are the factors that drive event attendance. Here, we seek to deploy models to analyse the casual relationships between event attendance and such factors such as the location of the event, timing during the semester and day, whether catering was provided, and more.
 
+## Task for Tom
+Hey Tom, thanks for agreeing to help out!
+
+In a nutshell, I need your help with scraping data from the [FB Graph Api](https://developers.facebook.com/docs/graph-api/). I have already scraped data from USYD Website and FB of who the biggest societies at USYD are, that's listed in the final_data.csv file. I'll need you to scrape data on these top 20 societies via Facebook, I've provided their FB id's to help you. Basically, I need you to scrape all the events that these societies have held for past few years (as much as you can get). Alongside that, I'd like to collect information about the event including: number of people who attended, people who said maybe, people who said no, name of event, date of when event was held, location of event, description of event, number of people invited to event, start time of event, and anything else you find interesting. What we like to then do with this data is do some data science work on seeing what factors drive event's attendance at USYD. Afterwards, I'd like you to write everything out to a csv file, where since we will have multiple events for each society scraped, have it so that you also create a new field called Society so we know which society hosted which events, and the rest of the fields are just simply each piece of information you are able to scrape.
+
+Here's a [good link](https://developers.facebook.com/tools/explorer/?method=GET&path=180DegreesConsulting&version=v2.12) of using FB Graph API explorer to look at things. You can use my token to access it which is in the file already.
+
+Primarily I've started some things with facebook_final_scrape.py file so you can take a look there. You can look at facebook_initial_scrape.py on some ways I've used to interact with FB Graph API to scrape the initial data.
+
+Thanks again!!!
 ## Requirements
 
 All packages used are in the [requirements.txt](https://github.com/chrishyland/USYD-Societies-Events/blob/master/requirements.txt) file.
--- facebook_final_scrape.py
+++ facebook_final_scrape.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 
 
 def connect(page_id, token):
-    '''Connect to FB Graph API and scrape data regarding events for each society.'''
+    '''Connect to FB Graph API and get data about society events.'''
     base = "https://graph.facebook.com/v2.11/"
     test_file = open('Test.csv', 'w')
     writer = csv.writer(test_file)
@@ -13,7 +13,10 @@ def connect(page_id, token):
     url = base + page_id + parameters
 
     data = json.loads(urlopen(url).read())
-    writer.writerow(["Society", "event_name", "attendance_count", "interested_count", "no_reply_count", "declined_count", "maybe_count", "total_invited_count", "description", "start_time", "end_time", "place", "event_type" ])
+    writer.writerow(["Society", "event_name", "attendance_count",
+                     "interested_count", "no_reply_count", "declined_count",
+                     "maybe_count", "total_invited_count", "description",
+                     "start_time", "end_time", "place", "event_type" ])
 
     # What this return is a dictionary in a list in a dictionary in a dictionary.
     for i in range(0, len(data['events']['data'])):
@@ -32,11 +35,15 @@ def connect(page_id, token):
             place = data_obj['place']
             type_of_event = data_obj['type']
 
-            writer.writerow(["180DC", name, attendance_count, interested_count, no_reply_count, declined_count, maybe_count, total_invited_count, description, start_time, end_time, place, type_of_event])
+            writer.writerow(["180DC", name, attendance_count, interested_count,
+                             no_reply_count, declined_count, maybe_count,
+                             total_invited_count, description, start_time,
+                             end_time, place, type_of_event])
         except Exception as e:
             continue
 
     test_file.close()
+
 
 def read_data():
     '''Open CSV file and pass next entry to FB graph API.'''
@@ -52,7 +59,7 @@ def main():
     token = 'EAACsR5scK9sBAPzJdyZBuiw7TBqfFMwUYw7qbZAuVI2FrRegxxdfsTqFPS51wF1NFklyV6ZA5HLHz7vAjRJwzPblbnrGiY6sfulSlqZBZASf1ZBG7qQcRjozmHGLlmajZCGNqClEYHZB7EOk6HrFRuEKZAc6MpZB6fZAgoZD'
 
     connect("180DegreesSydneyUniversity", token)
-    #read_data()
+    read_data()
 
 
     #"https://graph.facebook.com/v2.11/180DegreesConsulting/?fields=events.limit(10)%7Battending_count%2Ccategory%2Cdeclined_count%2Cdescription%2Cend_time%2Cevent_times%2Cinterested_count%2Cmaybe_count%2Cname%2Cnoreply_count%2Cplace%2Cscheduled_publish_time%2Cstart_time%2Cticket_uri%2Ctype%7D&access_token=EAACEdEose0cBAAnZB6jhSiHFzwzapuTxDdDvbX8vxMOLUh2lTWIICBBZBJw2tsGr46Ea62pBLxjPO9EJlXBet8dgrsNZCBrBrMU2m20o9N7KRACM1fGRYDrPHhMzsXWx6mplOsZCWWd22lb1qjqxwU3WQuqIVpOaBDIaJYXxBSTADBM8xid0JAqQ6ZA3rtcmINDEikymjJwZDZD"
